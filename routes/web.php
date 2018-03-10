9d2b044d2ac880ff1c70b3ff757161cea6cb988f
Ray <ray247k@gmail.com>
2018-03-10 22:09:12 +0800

    add: my first page
    新增一個自己的頁面

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * 指定一個新的路由到domain/about下，載入about.blade.php作為view
+ */
+Route::get('about', function () {
+    return view('about');
+});
