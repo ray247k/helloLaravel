3771bfc5ad9ab7009245618975255f93c06b5ec8
Qian <ray247k@gmail.com>
2018-04-08 16:14:56 +0800

    change: 分別引入頁面內容

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,13 +1,26 @@
 <!doctype html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Layout練習</title>
 
-    <title>Layout分解</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+    <!-- Custom styles for this template -->
+    <link href="/css/album.css" rel="stylesheet">
 </head>
+
 <body>
 
+@include('layouts.nav')
+
+<div class="container">
     @yield('content')
+</div>
+
+@include('layouts.footer')
 
 </body>
-
 </html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.layout')
 
 @section('content')
-    content 內容
+    <main role="main">
+
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Lay out 使用 yield的內容</h1>
+                <p class="lead text-muted">section的content就是這堆東西，這是posts的首頁</p>
+                <p>
+                    <a href="#" class="btn btn-primary my-2">安安</a>
+                    <a href="#" class="btn btn-secondary my-2">你好</a>
+                </p>
+            </div>
+        </section>
+
+    </main>
 @endsection
