20022c7f3fd79da4bf4d9c3f6ceb14453941f834
Qian <ray247k@gmail.com>
2018-04-08 15:32:03 +0800

    add: layout for posts using blade

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<head>
+
+    <title>Layout分解</title>
+
+</head>
+<body>
+
+    @yield('content')
+
+</body>
+
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    content 內容
+@endsection
