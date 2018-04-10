637d4e0b77d41f0c2525f59e1ea84ba0ae10f247
Qian <ray247k@gmail.com>
2018-04-10 22:38:25 +0800

    add: 第一種寫入方法

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -27,9 +28,15 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $post = new Post;
+
+        // way 1
+        $post->title = request('title');
+        $post->body = request('body');
+        $post->save();
+        return redirect('/post');
     }
 
     public function show()
