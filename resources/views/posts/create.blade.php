839d8f07e171e8c57fe341ac07613dedf067dbb6
Qian <ray247k@gmail.com>
2018-04-10 22:28:27 +0800

    add: CSRF token

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,13 +4,13 @@
     <main role="main">
         <h1>發表文章</h1>
         <form method="post" action="/posts">
-            {{csrf_token()}}
+            {{ csrf_field() }}
             <div class="form-group">
-                <label for="exampleInputEmail1">標題</label>
+                <label for="title">標題</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="文章標題">
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">內文</label>
+                <label for="body">內文</label>
                 <textarea type="text" class="form-control" id="body" name="body" placeholder="文章內文"></textarea>
             </div>
             <button type="submit" class="btn btn-primary">送出</button>
