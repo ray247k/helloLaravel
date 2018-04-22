e8e249a7a72aeb42494dd46b726d90fff06c3107
Qian <ray247k@gmail.com>
2018-04-22 14:08:58 +0800

    add: posts page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts.master')
 
 @section('content')
-    <main role="main">
-
-        <section class="jumbotron text-center">
-            <div class="container">
-                <h1 class="jumbotron-heading">Lay out 使用 yield的內容</h1>
-                <p class="lead text-muted">section的content就是這堆東西，這是posts的首頁</p>
-                <p>
-                    <a href="#" class="btn btn-primary my-2">安安</a>
-                    <a href="#" class="btn btn-secondary my-2">你好</a>
-                </p>
-            </div>
-        </section>
-
-    </main>
+    <div class="col-sm-8 blog-main">
+        @foreach($posts as $post)
+            @include('posts.post')
+        @endforeach
+    </div>
 @endsection
