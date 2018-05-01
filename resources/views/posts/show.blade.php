efdfbdb917f2e6bdea5b72ef3dc5dca9456a231a
Qian <ray247k@gmail.com>
2018-05-01 16:10:26 +0800

    modify: show the comments

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,5 +4,17 @@
     <div class="col-sm-8 blog-main">
         <h1>{{ $post->title }}</h1>
         {{ $post->body }}
+
+        <hr>
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                   <li class="list-group-item">
+                       {{ $comment->body }}
+                   </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
