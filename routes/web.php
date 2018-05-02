2943df5cdeb323ed6a37e005171cc6e87be1e912
Qian <ray247k@gmail.com>
2018-05-02 22:47:50 +0800

    modify: routes

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,9 @@ Route::get('about', function () {
 });
 
 Route::get('/tasks', 'TasksController@index');
-
 Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/posts/create', 'PostsController@create');
-
 Route::get('/posts', 'PostsController@index');
 Route::get('/posts/{posts}', 'PostsController@show');
-
 Route::post('/posts', 'PostsController@store');
